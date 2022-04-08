0376b75eb64f7e71e4ab275e1a6d94a96c262cd2
xfaraday <theonlyfaraday@gmail.com>
2022-04-08 01:40:29 -0700

    first commit

---
+++ privesc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#suid perms
+chmod u+s /bin/chown
+chmod u+s /usr/bin/awk
+
+#adding vulnerable crontab
+echo "*/2 * * * * root /home/luke_skywalker/run.sh" > /etc/crontab
+echo -e "#!/bin/bash\n\nprintf 'hello'" > /home/luke_skywalker/run.sh
+
+#vulnerable path in script
+echo -e "#!/bin/bash\n\necho'testing internet'\ncurl -I http://google.com" > /home/luke_skywalker/internet.sh
+chown root /home/luke_skywalker/internet.sh
+chmod 555 /home/luke_skywalker/internet.sh
+chmod u+s /home/luke_skywalker/internet.sh
