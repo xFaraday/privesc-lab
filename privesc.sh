8eb6fd3ced65825df00c379fe78b5c0253263353
Ethan Michalak <TheOnlyFaraday@gmail.com>
2022-04-08 01:45:05 -0700

    Update

--- privesc.sh
+++ privesc.sh
@@ -7,6 +7,7 @@ chmod u+s /usr/bin/awk
 #adding vulnerable crontab
 echo "*/2 * * * * root /home/luke_skywalker/run.sh" > /etc/crontab
 echo -e "#!/bin/bash\n\nprintf 'hello'" > /home/luke_skywalker/run.sh
+chmod 777 /home/luke_skywalker/run.sh
 
 #vulnerable path in script
 echo -e "#!/bin/bash\n\necho'testing internet'\ncurl -I http://google.com" > /home/luke_skywalker/internet.sh
